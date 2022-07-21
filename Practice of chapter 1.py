c63a43b5dd57227eae89c0f2b34b0a830e429e52
rohanmandal07 <109741370+rohanmandal07@users.noreply.github.com>
2022-07-21 18:40:49 +0530

    Add files via upload

---
+++ Practice of chapter 1.py
@@ -0,0 +1,26 @@
+''' This is a practice set of chapter 1 '''
+# 001
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
+
+
+#002
+'''this answer is using window terminal '''
+
+
+#004
+
+import os
+print(os.listdir())
+
+#005
+'''author: Rohan
+Date: 14/06/2022'''
+
+import os
+print(os.listdir())
+
+
